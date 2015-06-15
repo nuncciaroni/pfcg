6dbdd98dd081299a1c2377411818c190dc8801f3
nuncciaroni <amanda.nuncciaroni@hotmail.com>
2015-06-14 21:37:32 -0300

    Tentativa falha de fazer o primeiro tópico extra
    A câmera estava detectando melhor os objetos antes dessa atualização e não foi possível detectar as coordenadas do objeto.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,17 @@
 #include "opencv2/nonfree/nonfree.hpp"
 #include "opencv2/nonfree/features2d.hpp"
 #include "opencv2/features2d/features2d.hpp"
+#include <math.h>
 
 using namespace cv;
 using namespace std;
+
+float Distance(Point2f p1, Point2f p2)
+{
+    float d = 0;
+    d = sqrt(((p2.x - p1.x)*(p2.x - p1.x)) + ((p2.y - p1.y)*(p2.y - p1.y)));
+    return d;
+}
 
 int main( int argc, char** argv )
 {
@@ -15,7 +23,7 @@ int main( int argc, char** argv )
 
     int minHessian = 400;
     Mat img;
-    vector<KeyPoint> keypoints;
+    vector<KeyPoint> keypoints, aglomerate;
 
     SurfFeatureDetector detector(minHessian);
     Mat imgOriginal;
@@ -42,22 +50,39 @@ int main( int argc, char** argv )
 
         inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
+        morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(5, 5)));
+        morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(5, 5)));
+
         detector.detect(imgThresholded, keypoints);
 
-        drawKeypoints(imgOriginal, keypoints, img, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
+        int tem = keypoints.size();
 
-        imshow("Thresholded Image", imgThresholded); //show the thresholded image
-        imshow("Original", img); //show the original image
-
-        cout << (keypoints.size() > 5 ? "objeto detectado" : " ") << endl;
-
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+    for(int i = 0; i < tem; i++)
+    {
+      for(int j = tem-1; j == i; j--)
+      {
+        if(Distance(keypoints[i].pt, keypoints[j].pt) < 300)
         {
-            cout << "esc key is pressed by user" << endl;
-            break;
+          aglomerate.push_back(keypoints[j]);
         }
+      }
     }
 
-    return 0;
+            drawKeypoints(imgOriginal, aglomerate, img, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
 
-}
+            imshow("Thresholded Image", imgThresholded); //show the thresholded image
+            imshow("Original", img); //show the original image
+
+            cout << (aglomerate.size() > 5 ? "objeto detectado" : " ") << endl;
+
+            if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+            {
+                cout << "esc key is pressed by user" << endl;
+                break;
+            }
+        }
+        keypoints.clear();
+
+        return 0;
+
+    }
